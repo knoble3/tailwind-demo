c207c3e6ee58e210fc5d90669a97a0435eddc595
Keegan Noble <keegannoble90@gmail.com>
2020-08-05 22:43:53 +0800

    config settings and added custom defaults

---
+++ app/javascript/css/tailwind.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  theme: { 
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+    extend: {
+      spacing: {
+        '96': '24rem',
+        '128': '32rem',
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
